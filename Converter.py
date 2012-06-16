40e8479f94b8cf5cf71c1f710007ec1d83bec1d2
Cristhofer Travieso <cristhofert97@gmail.com>
2012-06-15 22:17:13 -0300

    Cree la caja que del canvas, con dos combos y labels

--- Converter.py
+++ Converter.py
@@ -18,85 +18,23 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-import json
-
-from gettext import gettext as _
 import gtk
 
-from sugar import profile
-from sugar import mime
-from sugar.activity import activity
-from sugar.activity.widgets import StopButton
-from sugar.activity.widgets import ActivityToolbarButton
-from sugar.activity.widgets import ToolbarButton
-from sugar.graphics.icon import Icon
-from sugar.graphics.colorbutton import ColorToolButton
-from sugar.graphics.toolbarbox import ToolbarBox
-from sugar.graphics.toolbutton import ToolButton
-from sugar.graphics.radiotoolbutton import RadioToolButton
-from sugar.graphics.objectchooser import ObjectChooser
-from sugar.graphics.alert import Alert, NotifyAlert
+class Converter(gtk.VBox):
 
-class Activity(activity.Activity):
-    def __init__(self, handle):
-        activity.Activity.__init__(self, handle, True)
+    def __init__(self):
+        gtk.VBox.__init__(self)
 
-        toolbarbox = ToolbarBox()
+        self._hbox = gtk.VBox()
 
-        activity_button = ActivityToolbarButton(self)
+        self.combo1 = gtk.Combo()
 
-        toolbarbox.toolbar.insert(activity_button, 0)
+        self.combo2 = gtk.Combo()
 
-        separator = gtk.SeparatorToolItem()
-        separator.set_expand(False)
-        separator.set_draw(True)
-        toolbarbox.toolbar.insert(separator, -1)
+        self.add(gtk.Label("Comvertir"))
+        self._hbox(gtk.Label("De "))
+        self._hbox.add(self.combo1)
+        self._hbox.add(gtk.Label(" a "))
+        self._hbox.add(self.combo2)
 
-        # RadioToolButton
-        self._long_btn = RadioToolButton()
-        self._long_btn.props.icon_name = "long"
-
-        self._vol_btn = RadioToolButton()
-        self._vol_btn.props.icon_name = "vol"
-        self._vol_btn.props.group = self._long_btn
-
-        self._area_btn = RadioToolButton()
-        self._area_btn.props.icon_name = "area"
-        self._area_btn.props.group = self._long_btn
-
-        self._peso_btn = RadioToolButton()
-        self._peso_btn.props.icon_name = "peso"
-        self._peso_btn.props.group = self._long_btn
-
-        self._vel_btn = RadioToolButton()
-        self._vel_btn.props.icon_name = "vel"
-        self._vel_btn.props.group = self._long_btn
-
-        self._time_btn = RadioToolButton()
-        self._time_btn.props.icon_name = "time"
-        self._time_btn.props.group = self._long_btn
-
-        toolbarbox.toolbar.insert(self._long_btn, -1)
-        toolbarbox.toolbar.insert(self._vol_btn, -1)
-        toolbarbox.toolbar.insert(self._area_btn, -1)
-        toolbarbox.toolbar.insert(self._peso_btn, -1)
-        toolbarbox.toolbar.insert(self._vel_btn, -1)
-        toolbarbox.toolbar.insert(self._time_btn, -1)
-
-        #
-        separator = gtk.SeparatorToolItem()
-        separator.set_expand(True)
-        separator.set_draw(False)
-        toolbarbox.toolbar.insert(separator, -1)
-
-        stopbtn = StopButton(self)
-        toolbarbox.toolbar.insert(stopbtn, -1)
-
-        self.set_toolbar_box(toolbarbox)
-
-        #Canvas
-        canvas = gtk.VBox()
-
-        self.set_canvas(canvas)
-
-        self.show_all()
+        self.add(self._hbox)
