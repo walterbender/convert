78f32b16a5ab22d2def041c502245aa721b803b4
Cristhofer Travieso <cristhofert97@gmail.com>
2012-07-03 02:07:00 +0000

    cambie el comentario donde espesifica el autor

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Comverter.py by:
+# comverter.py by:
 #	Cristhofer Travieso <cristhofert97@gmail.com>
 
 # This program is free software; you can redistribute it and/or modify
