3dacbddde7f1ae4124a63ff18f3086e0950f2fb8
Cristhofer Travieso <cristhofert97@gmail.com>
2013-04-24 16:17:50 -0300

    import pango to from gi.repository import pango
    Signed-off-by: Cristhofer Travieso <cristhofert97@gmail.com>

--- activity.py
+++ activity.py
@@ -15,7 +15,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 from gi.repository import Gtk
-import pango
+from gi.repository import pango
 import locale
 import convert
 
