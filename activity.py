walterbender/convert

commit 98bce4f40a362aeadea6f7a9f6b2d292d6e6f997

Avoid deprecated Gtk.VBox and Gtk.HBox

activity.py
@@ -44,9 +44,6 @@ class ConvertActivity(activity.Activity):
         self.max_participants = 1
         self.dic = {}
 
-        # Canvas
-        self._canvas = Gtk.VBox()
-
         self._liststore = Gtk.ListStore(str)
         arrow_font = Pango.FontDescription('sans bold 18')
         input_font = Pango.FontDescription('sans 12')
@@ -85,9 +82,9 @@ class ConvertActivity(activity.Activity):
         self.ratio._size = 12
         self.ratio.connect('draw', self._ratio_draw_cb)
 
-        l_hbox = Gtk.HBox()
-        u_hbox = Gtk.HBox()
-        label_box = Gtk.HBox()
+        l_hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        u_hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        label_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
 
         self.label1 = Gtk.Label()
         self.label1.set_markup('<big>From value</big>')
@@ -109,11 +106,11 @@ class ConvertActivity(activity.Activity):
         l_hbox.pack_end(self.to_unit, True, True, 5)
         label_box.add(self.ratio)
 
-        self._canvas.pack_start(u_hbox, False, False, 30)
-        self._canvas.pack_start(l_hbox, False, False, 0)
-        self._canvas.pack_start(label_box, True, False, 0)
-
-        self.set_canvas(self._canvas)
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        box.pack_start(u_hbox, False, False, 30)
+        box.pack_start(l_hbox, False, False, 0)
+        box.pack_start(label_box, True, False, 0)
+        self.set_canvas(box)
 
         # Toolbar
         toolbarbox = ToolbarBox()

commit dfce6393627d26efd24a48f914fcee64de60850e

Fix ratio label on left edge
Ratio label had moved to left edge instead of being centered.

Regression identified by bisection, introduced by 98bce4f ("Avoid
deprecated Gtk.VBox and Gtk.HBox") because Gtk.Box is slightly
different.

activity.py
@@ -103,7 +103,6 @@ class ConvertActivity(activity.Activity):
 
         l_hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         u_hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        label_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
 
         self.label1 = Gtk.Label()
         self.label1.set_markup('<big>From value</big>')
@@ -123,12 +122,11 @@ class ConvertActivity(activity.Activity):
         l_hbox.pack_start(self.arrow, False, False, 15)
         l_hbox.pack_start(self.to_value, True, True, 5)
         l_hbox.pack_end(self.to_unit, True, True, 5)
-        label_box.add(self.ratio)
 
         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         box.pack_start(u_hbox, False, False, 30)
         box.pack_start(l_hbox, False, False, 0)
-        box.pack_start(label_box, True, False, 0)
+        box.pack_start(self.ratio, True, False, 0)
         self.set_canvas(box)
 
         # Toolbar